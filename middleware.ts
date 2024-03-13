eadd1164bf9108195dfde3e0f2dadd8149757f07
abderahim <abderrahimaneddam1@gmail.com>
2024-03-13 14:07:22 +0100

    commit

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,8 @@ import type { NextRequest } from "next/server";
 import { API_HOST } from "./utils/api";
 
 export async function middleware(request: NextRequest) {
-   let hasAccess = false;
   if (request.nextUrl.pathname.startsWith("/dashboard")) {
+   let hasAccess = false;
     let token = request.cookies.get("access_token")?.value;
     try {
       let res = await fetch(API_HOST + "auth/me", {
@@ -17,9 +17,9 @@ export async function middleware(request: NextRequest) {
       // TODO: Redirect to auth later
       hasAccess = false;
     }
-  }
-  if (!hasAccess) {
-    return NextResponse.redirect(request.nextUrl.origin);
+    if (!hasAccess) {
+      return NextResponse.redirect(request.nextUrl.origin);
+    }
   }
   const response = NextResponse.next();
   return response;
